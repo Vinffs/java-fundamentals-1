49a15bdd52d6bb9a5d0844539ffb4d1f75686de6
Vinffs <vin.demiranda@gmail.com>
2024-03-27 16:04:17 +0100

    Bonus: Simple version of name check inside the array

---
+++ src/org/experis/ferragnez/FerragnezParty.java
@@ -0,0 +1,31 @@
+package org.experis.ferragnez;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class FerragnezParty {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String[] invitedGuests = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary " +
+                "Blasi","Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        System.out.print("Good Evening! May I have your name? ");
+
+        String nameToCheck = scan.nextLine();
+
+        // Methods to check if nameToCheck is included in the invitedGuests array
+
+        // Method 1 -> asList method
+        List<String> namesList = Arrays.asList(invitedGuests);
+
+        if(namesList.contains(nameToCheck)) {
+            System.out.println("This way please!");
+        } else {
+            System.out.println("My apologies, your name is not on the list. I can't let you in.");
+        }
+
+    }
+}
