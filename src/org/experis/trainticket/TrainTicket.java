666799f340cf2a1d285e492606fc72dd151a770c
Vinffs <vin.demiranda@gmail.com>
2024-03-27 14:28:27 +0100

    store user input in variables km and age

--- src/org/experis/trainticket/TrainTicket.java
+++ src/org/experis/trainticket/TrainTicket.java
@@ -1,7 +1,16 @@
 package org.experis.trainticket;
 
+import java.util.Scanner;
+
 public class TrainTicket {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please insert mileage (km): ");
+        int km = scan.nextInt();
+        System.out.print("Please insert your age: ");
+        int age = scan.nextInt();
 
+        System.out.println(km);
+        System.out.println(age);
     }
 }
